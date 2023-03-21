da85b46ed1a91bc3ece1468c3f8475be0b862b23
JoeWantsABrew <joewantsabrew@yahoo.com>
2023-03-21 06:43:04 +0000

    Game Works Now

--- AI/Assets/Scripts/Other/UI & Start Menu/STartMenu.cs
+++ AI/Assets/Scripts/Other/UI & Start Menu/STartMenu.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class STartMenu : MonoBehaviour
 {
-    public List <string> GotoScene;
+    public string GotoScene;
     
     public void ThisScene()
     {
-        SceneManager.LoadScene(GotoScene[Random.Range(0, GotoScene.Count - 1)]);
+        SceneManager.LoadScene(GotoScene);
     }
 
     public void Quitt()
