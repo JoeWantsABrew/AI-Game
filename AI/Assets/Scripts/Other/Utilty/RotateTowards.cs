a11ac0d7608779fb9385c4d6a3fb42a270053c9c
JoeWantsABrew <joewantsabrew@yahoo.com>
2023-05-20 23:12:36 +0100

    mhm

---
+++ AI/Assets/Scripts/Other/Utilty/RotateTowards.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour
+{
+
+    public Transform Target;
+
+    void Update()
+    {
+
+        Vector2 Delta = (Target.position - transform.position).normalized;
+        transform.rotation = Quaternion.Euler(0, 0, (Mathf.Atan2(Delta.y, Delta.x)) * Mathf.Rad2Deg);
+    }
+}
