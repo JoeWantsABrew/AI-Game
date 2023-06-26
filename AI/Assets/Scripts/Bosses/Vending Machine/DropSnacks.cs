dd4923e79a72ec2859723a613693b6c1a6b3c733
JoeWantsABrew <joewantsabrew@yahoo.com>
2023-06-26 12:09:46 +0100

    yum yum

---
+++ AI/Assets/Scripts/Bosses/Vending Machine/DropSnacks.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropSnacks : MonoBehaviour
+{
+    public float StartDelay;
+    public float Intervals;
+    public GameObject[] Snackies;
+
+    public GameObject ChipRed;
+    public GameObject ChipGreen;
+    public GameObject ChipOrange;
+    public GameObject ColaRegular;
+    public GameObject ColaDiet;
+    public GameObject Chocolate;
+}
